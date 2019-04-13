b0df017a703eb11e115122a68bf325633737d90b
timlathy <tim.labushev@gmail.com>
2019-04-13 15:21:53 +0300

    30fps bun

--- mirror/index.js
+++ mirror/index.js
@@ -2,61 +2,110 @@
 
 // Coordinates: (0, 0) is the top left corner
 
+const TARGET_FPS = 30;
+const FPS_DELTA = 120 / TARGET_FPS; /* the ticker runs at 60 FPS */
+
+const resources = PIXI.loader.resources;
 const resourceDefs = [
-  { name: 'mirry_down_1', url: 'https://johnny-keker.github.io/random-web-game/res/mirry_down_1.png' }
+  { name: 'bg', url: 'https://images.pexels.com/photos/1932264/pexels-photo-1932264.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=600&w=900' },
+  { name: 'bun-left-1', url: 'res/bun-left-1.png' },
+  { name: 'bun-left-2', url: 'res/bun-left-2.png' },
+  { name: 'bun-left-3', url: 'res/bun-left-3.png' }
 ]
 
-let mirrySprite;
+let bun;
+let bunSprites;
 
 PIXI.loader.add(resourceDefs).load(() => {
-  const app = new PIXI.Application({ width: 800, height: 600 });
+  const app = new PIXI.Application({ width: 900, height: 600 });
 
-  const resources = PIXI.loader.resources;
-  mirrySprite = new PIXI.Sprite(resources['mirry_down_1'].texture);
-  mirrySprite.position.set(96, 96);
+  const bg = new PIXI.Sprite(resources['bg'].texture);
 
-  app.stage.addChild(mirrySprite);
+  bun = new PIXI.Sprite(resources['bun-left-1'].texture);
+  bun.position.set(96, 96);
 
-  app.ticker.add(frame);
+  app.stage.addChild(bg);
+  app.stage.addChild(bun);
+
+  app.ticker.add(frameTick);
 
   app.renderer.view.style.display = 'block';
   document.body.appendChild(app.view);
+
+  document.addEventListener('keydown', (e) => keyboard(e.key, true), false);
+  document.addEventListener('keyup', (e) => keyboard(e.key, false), false);
 });
 
 const state = {
-  vx: 0,
-  vy: 0
+  frameTick: 0,
+  direction: 'left',
+  vel: 0
 };
 
 function keyboard(key, pressed) {
-  switch (key) {
-    case 'ArrowUp':
-      if (pressed)
-        state.vx = 0;
-      state.vy = pressed ? -2 : 0;
-      break;
-    case 'ArrowDown':
-      if (pressed)
-        state.vx = 0;
-      state.vy = pressed ? 2 : 0;
-      break;
-    case 'ArrowLeft':
-      if (pressed)
-        state.vy = 0;
-      state.vx = pressed ? -2 : 0;
-      break;
-    case 'ArrowRight':
-      if (pressed)
-        state.vy = 0;
-      state.vx = pressed ? 2 : 0;
-      break;
+  if (!key.startsWith('Arrow'))
+    return;
+
+  if (!pressed)
+    state.vel = 0;
+  else {
+    state.vel = 4;
+    switch (key) {
+      case 'ArrowUp':
+        state.direction = 'up';
+        break;
+      case 'ArrowDown':
+        state.direction = 'down';
+        break;
+      case 'ArrowLeft':
+        state.direction = 'left';
+        break;
+      case 'ArrowRight':
+        state.direction = 'right';
+        break;
+    }
   }
 }
 
-document.addEventListener('keydown', (e) => keyboard(e.key, true), false);
-document.addEventListener('keyup', (e) => keyboard(e.key, false), false);
+/* https://www.pixiplayground.com/#/edit/oGvDC6wt~XQ_xBT6Ssb10 */
+let elapsed = 0;
+function frameTick(delta) {
+  elapsed += delta;
 
-function frame(delta) {
-  mirrySprite.x += state.vx;
-  mirrySprite.y += state.vy;
+  if (elapsed >= FPS_DELTA) {
+    frame();
+    elapsed = 0;
+  }
+}
+
+function frame() {
+  if (state.vel > 0) {
+    state.frameTick += 1;
+    if (state.frameTick === 1)
+      bun.texture = resources['bun-left-1'].texture;
+    else if (state.frameTick === 3)
+      bun.texture = resources['bun-left-2'].texture;
+    else if (state.frameTick === 6)
+      bun.texture = resources['bun-left-3'].texture;
+    else if (state.frameTick === 7)
+      state.frameTick = 0;
+
+    switch (state.direction) {
+      case 'up':
+        bun.y -= state.vel;
+        break;
+      case 'down':
+        bun.y += state.vel;
+        break;
+      case 'left':
+        bun.x -= state.vel;
+        break;
+      case 'right':
+        bun.x += state.vel;
+        break;
+    }
+  }
+  else {
+    bun.texture = resources['bun-left-1'].texture;
+  }
 }
